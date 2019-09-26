6f61a7950ea07f92cbf7fe4a8da4b5c43e2c29e4
Pedro Nascimento <nennogabriel@gmail.com>
2019-09-26 12:47:12 -0300

    10a13 Rota inicial

--- src/store/persistReducers.js
+++ src/store/persistReducers.js
@@ -1,15 +1,14 @@
-import storage from 'redux-persist/lib/storage';
+import AsyncStorage from '@react-native-community/async-storage';
 import { persistReducer } from 'redux-persist';
 
+const persistConfig = {
+  key: 'gobarber',
+  storage: AsyncStorage,
+  whitelist: ['auth', 'user'],
+};
+
 export default reducers => {
-  const persistedReducer = persistReducer(
-    {
-      key: 'gobarber',
-      storage,
-      whitelist: ['auth', 'user'],
-    },
-    reducers
-  );
+  const persistedReducer = persistReducer(persistConfig, reducers);
 
   return persistedReducer;
 };
