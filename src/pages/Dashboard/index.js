780cc29996b8ab172b898249a0073baad11f1f92
Pedro Nascimento <nennogabriel@gmail.com>
2019-09-26 14:15:01 -0300

    10a14 Estilização das rotas

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
-import { Text } from 'react-native';
 
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import Background from '~/components/Background';
 // import { Container } from './styles';
 
 export default function Dashboard() {
-  return <Text>Dashboard</Text>;
+  return <Background />;
 }
+
+Dashboard.navigationOptions = {
+  tabBarLabel: 'Agendamentos',
+  tabBarIcon: ({ tintColor }) => (
+    <Icon name="event" size={20} color={tintColor} />
+  ),
+};
